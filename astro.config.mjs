7d3124023a809f57baaacb0cd08e690db14b889b
Nils <fahrni.nils@gmail.com>
2026-01-05 17:21:49 +0100

    added deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://okaynils.github.io', 
+  base: 'based.health',
   vite: {
     plugins: [tailwindcss()],
   },
